85339a1cfd4e589f0be4a689fcbb44d7adfbc112
Ilmir <akmakk@yandex.ru>
2017-08-11 09:30:35 +0300

    feat: Product Gallery

---
+++ src/Product/Gallery/GalleryPhoto.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Item = styled.img`
+  width: auto;
+  height: 400px;
+  
+  @media screen and (min-width: 768px) {
+    height: 576px;
+  }
+`
+
+export default function GalleryPhoto(props) {
+  return (
+    <Item src={`img/${props.photoName}.png`} alt={props.alt}/>
+  )
+}
