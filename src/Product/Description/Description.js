bb50bdd84a59062376c9bf0e96aa7df5df19b919
Ilmir <akmakk@yandex.ru>
2017-08-12 16:18:29 +0300

    feat: description image desctop

--- src/Product/Description/Description.js
+++ src/Product/Description/Description.js
@@ -2,6 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import triangle from '../../files/triangle.svg';
 import DescContent from './DescContent';
+import Responsive from 'react-responsive';
+
+const Desctop = ({ children }) =>
+  <Responsive minWidth={992} children={children} />;
+const Mobile = ({ children }) =>
+  <Responsive maxWidth={991} children={children} />;
 
 const Description = styled.div`
   margin: 0;
@@ -10,6 +16,10 @@ const Description = styled.div`
   @media screen and (min-width: 768px) {
     border-bottom: none;
   }
+`;
+const Image = styled.img`
+  display: flex;
+  width: 100%;
 `;
 
 const DescSubmenu = styled.div`
@@ -33,6 +43,13 @@ const DescSubmenu = styled.div`
   }
 `;
 
+const Wrap = styled.div`
+  @media screen and (min-width: 992px) {
+    margin-top: 4rem;
+    margin-bottom: 4rem;
+  }
+`;
+
 const DescTitle = styled.h2`
   margin: 0;
   font-size: 1rem;
@@ -44,18 +61,33 @@ const DescTitle = styled.h2`
 
 export default function () {
   return (
-    <div className="container">
-      <Description>
-        <DescSubmenu active>
-          <DescTitle>DESCRIPTION</DescTitle>
-        </DescSubmenu>
+    <Wrap>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-lg-4">
+            <Description>
+              <DescSubmenu active>
+                <DescTitle>DESCRIPTION</DescTitle>
+              </DescSubmenu>
 
-        <DescContent/>
+              <DescContent/>
 
-        <DescSubmenu>
-          <DescTitle>SHIPPING &amp; RETURNS</DescTitle>
-        </DescSubmenu>
-      </Description>
-    </div>
+              <DescSubmenu>
+                <DescTitle>SHIPPING &amp; RETURNS</DescTitle>
+              </DescSubmenu>
+            </Description>
+          </div>
+          <Desctop>
+            <div className="col-lg-8">
+              <Image
+                src="img/product-image-4@2x.png"
+                alt="Product Image 2"
+              />
+            </div>
+          </Desctop>
+
+        </div>
+      </div>
+    </Wrap>
   )
 }
