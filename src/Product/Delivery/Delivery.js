2666cec46c8a6ce9edd1d2d50a8bae6a289913cd
Ilmir <akmakk@yandex.ru>
2017-08-12 15:59:52 +0300

    fix: adaptive delivery block

--- src/Product/Delivery/Delivery.js
+++ src/Product/Delivery/Delivery.js
@@ -15,12 +15,12 @@ export default function () {
   return (
     <Delivery>
       <div className="container">
-        <div className="row">
-          <div className="col-sm-7">
+        <div className="row middle-lg">
+          <div className="col-md-7 col-lg-7">
             <Image/>
           </div>
 
-          <div className="col-sm-5">
+          <div className="col-md-5 col-lg-offset-1 col-lg-4">
             <Information/>
           </div>
         </div>
