f4cea96a7b103d854c55e39505de15730a8bb1cd
Ilmir <akmakk@yandex.ru>
2017-08-14 08:23:32 +0300

    fix: Product simple architecture

--- src/Product/MainInfo/index.js
+++ src/Product/MainInfo/index.js
@@ -1,122 +1,54 @@
 import React from "react";
 import styled from "styled-components";
-import Button from "../../common/Button";
-import ButtonColor from "./ButtonColor";
+import Header from "./Header";
+import GalleryXsMd from "./GalleryXsMd";
+import Details from "./Details";
 import { Lg, XsMd } from "../../common/Responsive";
-import Info from "./Info";
-import Size from "./SelectSize";
-import Help from "./Help";
 
-const Details = styled.div`
-  padding: 1rem 1rem 0rem 1rem;
-
-  @media screen and (min-width: 768px) {
-    padding: 0;
-  }
-`;
-
-const Color = styled.p`
-  margin: 0;
-  font-size: 0.75rem;
-  line-height: 0.875rem;
-  font-weight: 300;
-  color: #171717;
-`;
-
-const ColorValue = styled.span`font-weight: 700;`;
-
-const Select = styled.div`
+const Wrapper = styled.div`
   @media (min-width: 992px) {
-    margin-right: 0.5rem;
-    display: flex;
-    justify-content: space-between;
+    margin-bottom: 4rem;
+    background-color: #d4bdad;
   }
 `;
 
-const ColorPicker = styled.div`
+const MainImage = styled.img`
   display: flex;
-  border-bottom: 1px solid #c6c6c6;
-  padding-top: 1rem;
-  padding-bottom: 2rem;
-
-  @media (min-width: 992px) {
-    border-bottom: none;
-  }
+  width: 488px;
+  height: 651px;
+  margin: 0 -0.5rem;
 `;
 
-const ActionWrapper = styled.div`
-  margin-top: 2rem;
-  display: flex;
-  flex-direction: column;
-`;
-
-const ColumnWrapper = styled.div`
-  @media (min-width: 992px) {
-    width: 50%;
-    margin-right: 1rem;
-  }
-`;
-
-const DeliveryInfo = styled.div`margin-top: 0.5rem;`;
-
-const Title = styled.h3`
-  margin: 0;
-  padding: 0;
-  margin-bottom: 0.25rem;
-  font-weight: bold;
-  font-size: 0.75rem;
-  line-height: 0.875rem;
-  color: #171717;
-`;
-
-const Description = styled.div`
-  font-size: 0.75rem;
-  line-height: 1rem;
-  color: #171717;
-`;
-
-export default function() {
+export default () => {
   return (
-    <Details>
-      <Info />
+    <Wrapper>
+      <div className="container">
+        <XsMd>
+          <Header label="Long Cotton Gabardine Car Coat" />
+        </XsMd>
 
-      <Select>
-        <ColumnWrapper>
-          <Color>
-            Colour: <ColorValue>Honey</ColorValue>
-          </Color>
-          <ColorPicker>
-            <ButtonColor color="#232122">color: Black</ButtonColor>
-            <ButtonColor color="#cfa880" active>color: Honey</ButtonColor>
-          </ColorPicker>
-          <Lg>
-            <Button type="button" primary>ADD TO BAG</Button>
-          </Lg>
-        </ColumnWrapper>
-        <Lg>
-          <Size />
-        </Lg>
-      </Select>
+        <div className="row middle-lg">
+          <div className="col-xs-12 col-md-7 col-lg-6">
+            <XsMd>
+              <GalleryXsMd />
+            </XsMd>
 
-      <Lg>
-        <DeliveryInfo>
-          <Title>Free Next Day Delivery</Title>
-          <Description>
-            Order before 7pm Monday to Thursday for delivery the next day
-          </Description>
-        </DeliveryInfo>
-      </Lg>
+            <Lg>
+              <MainImage
+                src="img/product-image-1@2x.png"
+                alt="Product Image"
+              />
+            </Lg>
+          </div>
+          <div className="col-xs-12 col-md-5 col-lg-6">
+            <Lg>
+              <Header label="Long Cotton Gabardine Car Coat Coat Coat Coat Coat" />
+            </Lg>
 
-      <XsMd>
-        <ActionWrapper>
-          <Button type="button" primary>SELECT A SIZE</Button>
-          <Button type="button">FIND IN STORE</Button>
-        </ActionWrapper>
-      </XsMd>
-
-      <XsMd>
-        <Help />
-      </XsMd>
-    </Details>
-  );
+            <Details />
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
--- src/Product/index.js
+++ src/Product/index.js
@@ -1,61 +1,16 @@
 import React from "react";
-import styled from "styled-components";
-import Header from "./MainInfo/Header";
-import GalleryXsMd from "./MainInfo/GalleryXsMd";
-import MainInfo from "./MainInfo";
+import Product from "./MainInfo/index";
 import Description from "./Description";
 import Delivery from "./Delivery";
 import Recommend from "./Recommend/";
 import More from "./More";
 import GalleryLg from "./MainInfo/GalleryLg";
-import { Lg, XsMd } from "../common/Responsive";
-
-const Show = styled.div`
-  @media (min-width: 992px) {
-    margin-bottom: 4rem;
-    background-color: #d4bdad;
-  }
-`;
-
-const MainImage = styled.img`
-  display: flex;
-  width: 488px;
-  height: 651px;
-  margin: 0 -0.5rem;
-`;
+import { Lg } from "../common/Responsive";
 
 export default function() {
   return (
     <main>
-      <Show>
-        <div className="container">
-          <XsMd>
-            <Header label="Long Cotton Gabardine Car Coat" />
-          </XsMd>
-
-          <div className="row middle-lg">
-            <div className="col-xs-12 col-md-7 col-lg-6">
-              <XsMd>
-                <GalleryXsMd />
-              </XsMd>
-
-              <Lg>
-                <MainImage
-                  src="img/product-image-1@2x.png"
-                  alt="Product Image"
-                />
-              </Lg>
-            </div>
-            <div className="col-xs-12 col-md-5 col-lg-6">
-              <Lg>
-                <Header label="Long Cotton Gabardine Car Coat Coat Coat Coat Coat" />
-              </Lg>
-
-              <MainInfo />
-            </div>
-          </div>
-        </div>
-      </Show>
+      <Product />
       <Description />
       <Lg>
         <GalleryLg />
