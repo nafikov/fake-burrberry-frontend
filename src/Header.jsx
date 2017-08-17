0740fd085449e5544ba22689d704ae8679ff41d9
Ilmir <akmakk@yandex.ru>
2017-08-17 08:36:03 +0300

    feat: Country button

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import logo from './assets/logo.svg';
 
-const Header = styled.header`border-bottom: 1px solid #c6c6c6;`;
+const Header = styled.header``;
 
 const Logo = styled.img`
   display: block;
@@ -10,7 +10,6 @@ const Logo = styled.img`
   height: 10px;
   margin: 0 auto;
   padding: 1rem;
-  yarn upgrade eslint-plugin-jsx-a11y@^4.0.0 --dev
 
   @media screen and (min-width: 768px) {
     padding: 1.5rem;
@@ -23,9 +22,29 @@ const Logo = styled.img`
   }
 `;
 
-export default () =>
-  (<Header>
+const Country = styled.button`
+  // display: none;
+
+  @media screen and (min-width: 768px) {
+    position: absolute;
+    top: 1.5rem;
+    left: 0.5rem;
+    border: none;
+    padding: 0;
+    color: #999999;
+    font-size: 0.75rem;
+    line-height: 1rem;
+    font-family: Raleway, sans-serif;
+    font-weight: 700;
+    background-color: transparent;
+  }
+`;
+
+export default () => (
+  <Header>
+    <Country>Shopping in: United Kingdom(&#163;)</Country>
     <a href="/">
       <Logo src={logo} />
     </a>
-  </Header>);
+  </Header>
+);
