1fe0e6e7438a0acc9a392e1eb905f84ff8419ee6
Ilmir <akmakk@yandex.ru>
2017-08-18 10:41:55 +0300

    fix: return registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import 'normalize.css';
 import 'flexboxgrid2/dist/flexboxgrid.css';
 import './index.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
