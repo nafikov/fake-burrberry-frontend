a2eefa083e90fab75296cbebc4a6687037d65219
Ilmir <akmakk@yandex.ru>
2017-08-20 08:18:15 +0300

    fix: Header nav

--- src/Header/Navigation.jsx
+++ src/Header/Navigation.jsx
@@ -9,9 +9,14 @@ const LinkStyled = styled(NavLink)`
   font-family: Raleway, sans-serif;
   font-weight: 700;
   letter-spacing: 1.5px;
-  color: ${props => (props.active ? '#171717' : '#999999')};
+  color: #999999;
   text-decoration: none;
-  border-bottom: ${props => (props.active ? 'solid 1px #171717;' : 'none')};
+  border-bottom: 1px solid transparent;
+  
+  &.active {
+    color: #171717;
+    border-bottom: 1px solid #171717;
+  }
 `;
 
 const Wrapper = styled.div`
@@ -26,7 +31,7 @@ const Wrapper = styled.div`
 export default () => (
   <Wrapper>
     <LinkStyled to="/catalog/women/">WOMEN</LinkStyled>
-    <LinkStyled active to="/catalog/men/">MEN</LinkStyled>
+    <LinkStyled to="/catalog/men/">MEN</LinkStyled>
     <LinkStyled to="/catalog/children/">CHILDREN</LinkStyled>
     <LinkStyled to="/catalog/beauty/">BEAUTY</LinkStyled>
     <LinkStyled to="/catalog/experience/">EXPERIENCE</LinkStyled>
